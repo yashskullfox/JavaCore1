244ffe4f145a28ab7f015adcbc971ebb883b1633
Yash <skullfox@hackermail.com>
2019-01-11 12:00:50 -0600

    Java_Basic

--- intro/src/Day3_4/student1.java
+++ intro/src/Day3_4/student1.java
@@ -1,36 +1,47 @@
-package Day2_4_5;
+package Day3_4;
 
 public class student1 
 {
 private int studentID;
 private int sID = 545;
 private String studentName;
+private String firstName;
+private String lastName;
 private String studentType;
 
-student1()
+/*student1()
 {
-studentID = sID; sID++;
+	sID++;
+	studentID = sID; 
+
 studentType = "f";
 String studnetName = "test";
-}
-student1(char sType, String sName, String sType1)
+}*/
+
+student1(char sType,  String firstName, String lastName)
 {
-	studentID = sID; sID++;
-	studentType = sType1;
-	studentName = sName+sType1;
+	sID++;
+	studentID = sID;
+//	studentName = firstName+lastName;
+	this.setStudentName(firstName, lastName);
 }
- public void displayDetails(student1 std)
+public void setStudentName( String firstName, String lastName) {
+	this.studentName = firstName+lastName;
+} 
+public void displayDetails(student1 std)
  {
 	 System.out.println(std.studentID);
-	 System.out.println(std.studentType);
+	 // System.out.println(std.studentType);
 	 System.out.println(std.studentName);
  }
+ 
+
 public static void main(String args[])
 {
-	student1 displayValues = new student1();
 	student1 studentOne = new student1 ('D',"Boney","Thomas");
 	studentOne.displayDetails(studentOne);
 	student1 studentTwo = new student1('H',"Dinial","Bose");
 	studentTwo.displayDetails(studentTwo);
+	
 }
 }
