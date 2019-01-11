9d292449651e33dbf4989bb2d24db5be6a1ea7e1
Yash <skullfox@hackermail.com>
2019-01-11 00:20:52 -0600

    Java Advance

---
+++ intro/src/assignment8_9_10/wells.java
@@ -0,0 +1,14 @@
+package assignment8_9_10;
+
+public class wells 
+{
+
+
+public static void main (String[] args) {
+
+	EmployeeGrade ram = new EmployeeGrade(102,"Ram",4.1f,4.5f,2.4f);
+	ram.calculateAverageFeedback();
+	ram.calculateGrade();
+	ram.display();
+}
+	}
