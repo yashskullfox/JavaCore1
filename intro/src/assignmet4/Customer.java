c5cdc63df83b24501e48c7340e8c65f187a72285
Yash <skullfox@hackermail.com>
2019-01-19 11:07:26 -0600

    Java_Advance

--- intro/src/assignmet4/Customer.java
+++ intro/src/assignmet4/Customer.java
@@ -1,22 +1,12 @@
 package assignmet4;
 
-/*
-* This java file is a demo Java program which depicts a class
-* with a main method inside and necessary instance variables */
-/**
-* This class is template to create Customer Object.
-* Date : <<Replace with the date you do this assignment>> * @author <<Replace with your Emp No. & Name>>
-* @version 1.0
-*/
+
 public class Customer {
    private String customerID;
    private String customerName;
    private String address;
    private int pinCode;
-/**
-* This method creates an object of Customer Class and sets its *instance variable and display the same.
-* @param args The command line arguments
-*/
+
 public static void main (String [] args){
    Customer customer1 = new Customer();						 //statement 1
    customer1.customerID = "1234";   						//statement 2
